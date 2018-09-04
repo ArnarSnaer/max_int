cc8de5680e937d0c9cf7697f6882d3e9acfa2ec2
Arnar Snaer <arnarsnaer98@gmail.com>
2018-09-04 13:54:07 +0000

    First commit

---
+++ max_int.py
@@ -0,0 +1,5 @@
+# Find the maximum input by a user. No limit until user inputs negative number.
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+print("The maximum is", max_int)    # Do not change this line
